b0f9d79c4d94cad219683270f07a8ee5b551e363
little-24 <hoai2410404@gmail.com>
2025-09-19 23:07:02 +0700

    Hoai add Property

--- Jewelry/Component/dgvProperty.Designer.cs
+++ Jewelry/Component/dgvProperty.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Jewelry.Components
+﻿namespace Jewelry
 {
     partial class dgvProperty
     {
@@ -33,7 +33,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -55,7 +55,7 @@
             this.guna2DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             this.guna2DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
-            this.ProductName,
+            this.Name,
             this.Count});
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
@@ -103,9 +103,9 @@
             // 
             // Name
             // 
-            this.ProductName.HeaderText = "Name";
-            this.ProductName.MinimumWidth = 6;
-            this.ProductName.Name = "Name";
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
             // 
             // Count
             // 
@@ -119,7 +119,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.guna2DataGridView1);
             this.Name = "dgvProperty";
-            this.Size = new System.Drawing.Size(770, 620);
+                this.Size = new System.Drawing.Size(770, 620);
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -129,7 +129,7 @@
 
         private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
         private System.Windows.Forms.DataGridViewTextBoxColumn Count;
     }
 }
--- Jewelry/Component/dgvProperty.cs
+++ Jewelry/Component/dgvProperty.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Guna.UI2.WinForms;
 
-namespace Jewelry.Components
+namespace Jewelry
 {
     public partial class dgvProperty : UserControl
     {
         public dgvProperty()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // dgvProperty
+            // 
+            this.Name = "dgvProperty";
+            this.Load += new System.EventHandler(this.dgvProperty_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void dgvProperty_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
