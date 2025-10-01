5c98ed097b5e373c4d9640b5b628dbeae533cbbd
little-24 <hoai2410404@gmail.com>
2025-10-01 12:35:19 +0700

    thieu button submit hihi

--- Jewelry/Login.Designer.cs
+++ Jewelry/Login.Designer.cs
@@ -41,29 +41,24 @@
             this.cbxRole = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-
             this.lblName = new System.Windows.Forms.Label();
-
-
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picLogin)).BeginInit();
             this.panel5.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
-           
-
+            // 
+            // panel1
+            // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel1.Controls.Add(this.picLogin);
-
-
             this.panel1.Controls.Add(this.btnSubmit);
             this.panel1.Controls.Add(this.panel5);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.label1);
-
             this.panel1.Location = new System.Drawing.Point(458, 234);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(577, 728);
@@ -84,16 +79,17 @@
             // btnSubmit
             // 
             this.btnSubmit.Font = new System.Drawing.Font("EB Garamond", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(80, 620);
-
+            this.btnSubmit.Location = new System.Drawing.Point(80, 613);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(418, 51);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
             // 
             // panel5
             // 
             this.panel5.Controls.Add(this.txtPassword);
             this.panel5.Controls.Add(this.label4);
-
             this.panel5.Location = new System.Drawing.Point(59, 453);
-
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(462, 130);
             this.panel5.TabIndex = 3;
@@ -124,7 +120,7 @@
             this.label4.Font = new System.Drawing.Font("EB Garamond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(14, 12);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 39);
+            this.label4.Size = new System.Drawing.Size(115, 40);
             this.label4.TabIndex = 0;
             this.label4.Text = "Password";
             // 
@@ -132,9 +128,7 @@
             // 
             this.panel4.Controls.Add(this.txtUsername);
             this.panel4.Controls.Add(this.label3);
-
             this.panel4.Location = new System.Drawing.Point(59, 274);
-
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(462, 149);
             this.panel4.TabIndex = 2;
@@ -165,7 +159,7 @@
             this.label3.Font = new System.Drawing.Font("EB Garamond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(14, 17);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 39);
+            this.label3.Size = new System.Drawing.Size(126, 40);
             this.label3.TabIndex = 0;
             this.label3.Text = "Username";
             // 
@@ -173,9 +167,7 @@
             // 
             this.panel3.Controls.Add(this.cbxRole);
             this.panel3.Controls.Add(this.label2);
-
             this.panel3.Location = new System.Drawing.Point(59, 116);
-
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(462, 129);
             this.panel3.TabIndex = 1;
@@ -187,8 +179,7 @@
             this.cbxRole.FormattingEnabled = true;
             this.cbxRole.Location = new System.Drawing.Point(21, 58);
             this.cbxRole.Name = "cbxRole";
-
-            this.cbxRole.Size = new System.Drawing.Size(418, 58);
+            this.cbxRole.Size = new System.Drawing.Size(418, 43);
             this.cbxRole.TabIndex = 1;
             // 
             // label2
@@ -197,14 +188,13 @@
             this.label2.Font = new System.Drawing.Font("EB Garamond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(14, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 39);
+            this.label2.Size = new System.Drawing.Size(69, 40);
             this.label2.TabIndex = 0;
             this.label2.Text = "Role";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-
             this.label1.Font = new System.Drawing.Font("EB Garamond Medium", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(213, 23);
             this.label1.Name = "label1";
@@ -223,8 +213,6 @@
             this.lblName.Size = new System.Drawing.Size(739, 169);
             this.lblName.TabIndex = 8;
             this.lblName.Text = "Arpels Jewelry";
-
-
             // 
             // Login
             // 
