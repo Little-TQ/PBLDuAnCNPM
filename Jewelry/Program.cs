9b9c3c915112e594e44df58f227b1982050557c4
little-24 <hoai2410404@gmail.com>
2025-10-05 23:44:09 +0700

    commit

--- Jewelry/Program.cs
+++ Jewelry/Program.cs
@@ -18,7 +18,7 @@ namespace Jewelry
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             SetProcessDPIAware();
-            Application.Run(new UpdatePrice());
+            Application.Run(new HomePage());
         }
         [DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
