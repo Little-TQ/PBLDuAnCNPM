17a3c92ffcbf560f9a6c9e5ef35530c827989098
Little-TQ <tranthiquynh7112004@gmail.com>
2025-10-05 14:45:08 +0700

    Quynh add Update Price

---
+++ Jewelry/DTO/UpdateDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jewelry.DTO
+{
+    public class UpdateDTO
+    {
+        public string idUpdate { get; set; }
+        public string idMaterial { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public decimal Price { get; set; }
+        public decimal ChangePrice { get; set; }
+        public string MaterialName { get; set; } // Thêm để hiển thị tên chất liệu
+
+        public UpdateDTO() { }
+
+        public UpdateDTO(string idUpdate, string idMaterial, DateTime updateTime, decimal price, decimal changePrice)
+        {
+            this.idUpdate = idUpdate;
+            this.idMaterial = idMaterial;
+            this.UpdateTime = updateTime;
+            this.Price = price;
+            this.ChangePrice = changePrice;
+        }
+    }
+}
